ac8fc7d19e06517cba297e3d824098b33b36a844
Matías Steinberg <matiuri@hotmail.com>
2015-12-19 18:09:32 -0300

    Creates the Title Screen

--- core/src/kotlin/mati/nave/Game.kt
+++ core/src/kotlin/mati/nave/Game.kt
@@ -1,10 +1,36 @@
 package mati.nave
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import mati.advancedgdx.AdvancedGame
+import mati.advancedgdx.assets.FontLoader.FontLoaderParameter
+import mati.nave.screens.TitleScreen
+import kotlin.properties.Delegates
 
 class Game() : AdvancedGame() {
     override fun create() {
         super.create()
         init(this)
+        astManager.queue("ButtonUp", "GUI/ButtonUp.png", Texture::class).queue("ButtonDown", "GUI/ButtonDown.png",
+                Texture::class).queue("UbuntuRGen", "fonts/Ubuntu-R.ttf", FreeTypeFontGenerator::class)
+                .queue("UbuntuR-64", "ubuntur64", BitmapFont::class, FontLoaderParameter(astManager["UbuntuRGen"]) {
+                    it.color = Color.WHITE
+                    it.borderColor = Color.BLACK
+                    it.borderWidth = 2.5f
+                    it.size = 64
+                }).load {
+            scrManager.add("Title", TitleScreen(this)).load("Title").change("Title")
+        }
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1f)
+    }
+
+    override fun render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        super.render()
     }
 }
---
+++ core/src/kotlin/mati/nave/screens/TitleScreen.kt
@@ -0,0 +1,66 @@
+package mati.nave.screens
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+import mati.advancedgdx.screens.Screen
+import mati.advancedgdx.AdvancedGame.Static.log
+import mati.advancedgdx.utils.addListener1
+import mati.advancedgdx.utils.createButton
+import mati.advancedgdx.utils.createLabel
+import mati.advancedgdx.utils.createNPD
+import mati.nave.Game
+import kotlin.properties.Delegates
+
+class TitleScreen(game: Game) : Screen(game) {
+    private var stage: Stage by Delegates.notNull<Stage>()
+    private var up: NinePatchDrawable by Delegates.notNull<NinePatchDrawable>()
+    private var down: NinePatchDrawable by Delegates.notNull<NinePatchDrawable>()
+
+    override fun load() {
+        up = createNPD(game.astManager["ButtonUp", Texture::class], 10, 10, 10, 10)
+        down = createNPD(game.astManager["ButtonDown", Texture::class], 10, 10, 10, 10)
+    }
+
+    override fun show() {
+        stage = Stage(ScreenViewport())
+        val table: Table = Table()
+        stage.addActor(table)
+        table.setFillParent(true)
+        table.pad(10f)
+
+        val font = game.astManager["UbuntuR-64", BitmapFont::class]
+        table.add(createLabel("A la Nave", font, Color.GOLD)).colspan(2).expandX().pad(5f)
+        table.row()
+
+        //TODO: Use locales
+        val play: TextButton = createButton("Play", font, up, down)
+        play.addListener1 { e, a ->
+            log.e("${this.javaClass.simpleName}", "Not Yet Implemented")
+        }
+        table.add(play).expandX().fill().pad(5f)
+
+        val exit: TextButton = createButton("Exit", font, up, down)
+        exit.addListener1 { e, a ->
+            Gdx.app.exit()
+        }
+        table.add(exit).expandX().fill().pad(5f)
+
+        Gdx.input.inputProcessor = stage
+    }
+
+    override fun render(delta: Float) {
+        stage.act(delta)
+        stage.draw()
+    }
+
+    override fun hide() {
+        stage.dispose()
+    }
+}
